7a88c9c12526e2f1871df9ea99013a0c1819c9a0
GazizX <162038885+GazizX@users.noreply.github.com>
2025-03-11 17:23:28 +0300

    post request done

--- internal/database/db.go
+++ internal/database/db.go
@@ -1,7 +1,12 @@
 package database
 
 import (
+	"bytes"
+	"encoding/json"
 	models "fixitpolytech/internal/models"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 //метод для получения всех корпусов(нужно реализовать через подключение к бд)
@@ -22,11 +27,34 @@ import (
 	}
 	return buildings
 }*/
+
+func PostRequest(req *models.Request) error {
+	// Преобразуем запрос в JSON
+	payload, err := json.Marshal(*req)
+	if err != nil {
+		log.Println("Ошибка при преобразовании данных в JSON:", err)
+		return err
+	}
+	//Создаем POST-запрос
+	response, err := http.Post("http://localhost:3000/api/requests", "application/json", bytes.NewBuffer(payload))
+	if err != nil {
+		log.Println("Ошибка при отправке запроса:", err)
+		return err
+	}
+	defer response.Body.Close()
+	//Проверяем ответ сервера
+	if response.StatusCode != http.StatusOK {
+		log.Printf("Сервер вернул статус: %s\n", response.Status)
+		return fmt.Errorf("Ошибка сервера: %s", response.Status)
+	}
+
+	return nil
+}
 func GetAllBuildings() []models.Building {
 	return []models.Building{
-		{BuildingID:0, Name: "11 корпус", Address: "улица Обручевых, 1", BldType: "stud"},
-		{BuildingID:1, Name: "9 корпус",  Address: "Политехническая улица, 21", BldType: "stud"},
-		{BuildingID:2, Name: "6 общага",  Address: "Улица Харченко, 16", BldType: "dorm"},
-		{BuildingID:3, Name: "3 общага",  Address: "Лесной проспект, 65к3", BldType: "dorm"},
+		{BuildingID: 0, Name: "11 корпус", Address: "улица Обручевых, 1", BldType: "stud"},
+		{BuildingID: 1, Name: "9 корпус", Address: "Политехническая улица, 21", BldType: "stud"},
+		{BuildingID: 2, Name: "6 общага", Address: "Улица Харченко, 16", BldType: "dorm"},
+		{BuildingID: 3, Name: "3 общага", Address: "Лесной проспект, 65к3", BldType: "dorm"},
 	}
 }
--- internal/handlers/request.go
+++ internal/handlers/request.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fixitpolytech/internal/database"
 	"fixitpolytech/internal/services"
 	"fmt"
 	"log"
@@ -10,140 +11,148 @@ import (
 )
 
 func RegisterRequestHandlers(bot *telebot.Bot, requestService *services.RequestService) {
-    bot.Handle(telebot.OnText, func(c telebot.Context) error {
-        return DescriptionHandler(c, requestService)
-    })
+	bot.Handle(telebot.OnText, func(c telebot.Context) error {
+		return DescriptionHandler(c, requestService)
+	})
 
-    bot.Handle(&telebot.InlineButton{Unique: "plotnik_job"}, func(c telebot.Context) error {
-        return sendJobOptions(c, 0)
-    })
+	bot.Handle(&telebot.InlineButton{Unique: "plotnik_job"}, func(c telebot.Context) error {
+		return sendJobOptions(c, 0)
+	})
 
-    bot.Handle(&telebot.InlineButton{Unique: "electrik_job"}, func(c telebot.Context) error {
-        return sendJobOptions(c, 1)
-    })
+	bot.Handle(&telebot.InlineButton{Unique: "electrik_job"}, func(c telebot.Context) error {
+		return sendJobOptions(c, 1)
+	})
 
-    bot.Handle(&telebot.InlineButton{Unique: "santehnik_job"}, func(c telebot.Context) error {
-        return sendJobOptions(c, 2)
-    })
+	bot.Handle(&telebot.InlineButton{Unique: "santehnik_job"}, func(c telebot.Context) error {
+		return sendJobOptions(c, 2)
+	})
 
-    bot.Handle(&telebot.InlineButton{Unique: "skip_photo"}, func(c telebot.Context) error {
-        return CompleteRequest(c, requestService)
-    })
+	bot.Handle(&telebot.InlineButton{Unique: "skip_photo"}, func(c telebot.Context) error {
+		return CompleteRequest(c, requestService)
+	})
 
-    bot.Handle(telebot.OnPhoto, func(c telebot.Context) error {
-        return HandlePhoto(c, requestService)
-    })
+	bot.Handle(telebot.OnPhoto, func(c telebot.Context) error {
+		return HandlePhoto(c, requestService)
+	})
 }
 
 func DescriptionHandler(c telebot.Context, requestService *services.RequestService) error {
-    // Получаем заявку из сессии пользователя
-    sessionMutex.Lock()
-    req, ok := sessions[c.Sender().ID]
-    sessionMutex.Unlock()
+	// Получаем заявку из сессии пользователя
+	sessionMutex.Lock()
+	req, ok := sessions[c.Sender().ID]
+	sessionMutex.Unlock()
 
-    if !ok {
-        return c.Send("Ошибка: заявка не найдена")
-    }
+	if !ok {
+		return c.Send("Ошибка: заявка не найдена")
+	}
 
-    req.AdditionalText = c.Text()
+	req.AdditionalText = c.Text()
 
-    // Обновляем заявку в сессии
-    sessionMutex.Lock()
-    sessions[c.Sender().ID] = req
-    sessionMutex.Unlock()
+	// Обновляем заявку в сессии
+	sessionMutex.Lock()
+	sessions[c.Sender().ID] = req
+	sessionMutex.Unlock()
 
-    keyboard := &telebot.ReplyMarkup{}
-    btnPl := keyboard.Data("Плотник", "plotnik_job")
-    btnEl := keyboard.Data("Электрик", "electrik_job")
-    btnSan := keyboard.Data("Сантехник", "santehnik_job")
+	keyboard := &telebot.ReplyMarkup{}
+	btnPl := keyboard.Data("Плотник", "plotnik_job")
+	btnEl := keyboard.Data("Электрик", "electrik_job")
+	btnSan := keyboard.Data("Сантехник", "santehnik_job")
 
-    keyboard.Inline(keyboard.Row(btnPl, btnEl, btnSan))
+	keyboard.Inline(keyboard.Row(btnPl, btnEl, btnSan))
 
-    return c.Send("Выберите, к какому типу относится ваша заявка:", keyboard)
+	return c.Send("Выберите, к какому типу относится ваша заявка:", keyboard)
 }
 
 func sendJobOptions(c telebot.Context, fieldID int) error {
-    sessionMutex.Lock()
-    req, ok := sessions[c.Sender().ID]
-    sessionMutex.Unlock()
+	sessionMutex.Lock()
+	req, ok := sessions[c.Sender().ID]
+	sessionMutex.Unlock()
 
-    if !ok {
-        return c.Send("Ошибка: заявка не найдена")
-    }
+	if !ok {
+		return c.Send("Ошибка: заявка не найдена")
+	}
 
-    req.FieldID = fieldID
-    c.Set("request", req)
+	req.FieldID = fieldID
+	c.Set("request", req)
 
-    keyboard := &telebot.ReplyMarkup{}
-    btnSkip := keyboard.Data("Пропустить", "skip_photo")
-    keyboard.Inline(keyboard.Row(btnSkip))
+	keyboard := &telebot.ReplyMarkup{}
+	btnSkip := keyboard.Data("Пропустить", "skip_photo")
+	keyboard.Inline(keyboard.Row(btnSkip))
 
-    return c.Send("При желании можете отправить фотографию проблемы (или нажмите 'Пропустить'):", keyboard)
+	return c.Send("При желании можете отправить фотографию проблемы (или нажмите 'Пропустить'):", keyboard)
 }
 
 func HandlePhoto(c telebot.Context, requestService *services.RequestService) error {
-    sessionMutex.Lock()
-    req, ok := sessions[c.Sender().ID]
-    sessionMutex.Unlock()
+	sessionMutex.Lock()
+	req, ok := sessions[c.Sender().ID]
+	sessionMutex.Unlock()
 
-    if !ok {
-        return c.Send("Ошибка: заявка не найдена")
-    }
+	if !ok {
+		return c.Send("Ошибка: заявка не найдена")
+	}
 
-    photo := c.Message().Photo
-    fileURL, err := GetFileURL(c.Bot(), photo.FileID)
-    if err != nil {
-        log.Printf("Не удалось получить url фото")
-    } else {
-        req.Photos = append(req.Photos, fileURL)
-    }
+	photo := c.Message().Photo
+	fileURL, err := GetFileURL(c.Bot(), photo.FileID)
+	if err != nil {
+		log.Printf("Не удалось получить url фото")
+	} else {
+		req.Photos = append(req.Photos, fileURL)
+	}
 
-    return CompleteRequest(c, requestService)
+	return CompleteRequest(c, requestService)
 }
 
 func CompleteRequest(c telebot.Context, requestService *services.RequestService) error {
-    // Получаем заявку из сессии пользователя
-    sessionMutex.Lock()
-    req, ok := sessions[c.Sender().ID]
-    sessionMutex.Unlock()
+	// Получаем заявку из сессии пользователя
+	sessionMutex.Lock()
+	req, ok := sessions[c.Sender().ID]
+	sessionMutex.Unlock()
 
-    var field string 
+	var field string
 
-    if !ok {
-        return c.Send("Ошибка: заявка не найдена")
-    }
+	if !ok {
+		return c.Send("Ошибка: заявка не найдена")
+	}
 
+	//Отправка данных на сервер
+	if err := database.PostRequest(req); err != nil {
+		c.Send("Ошибка: Не удалось отправить заявку на сервер")
+	}
 
-    // Создаем заявку в сервисе
-    request := requestService.CreateRequest(req.BuildingID, req.FieldID, req.AdditionalText, req.Photos)
+	// Создаем заявку в сервисе
+	request := requestService.CreateRequest(req.BuildingID, req.FieldID, req.AdditionalText, req.Photos)
 
-    if(request.FieldID == 0) {field = "Плотник"}
-	if(request.FieldID == 1) {field = "Электрик"}
-	if(request.FieldID == 2) {field = "Сантехник"}
+	if request.FieldID == 0 {
+		field = "Плотник"
+	}
+	if request.FieldID == 1 {
+		field = "Электрик"
+	}
+	if request.FieldID == 2 {
+		field = "Сантехник"
+	}
 
-    // Очищаем сессию пользователя
-    sessionMutex.Lock()
-    delete(sessions, c.Sender().ID)
-    sessionMutex.Unlock()
+	// Очищаем сессию пользователя
+	sessionMutex.Lock()
+	delete(sessions, c.Sender().ID)
+	sessionMutex.Unlock()
 
-    msg := fmt.Sprintf(
-        "Заявка #%d создана!\n"+
-            "Здание: %d\n"+
-            "URL фото: %s\n"+
-            "Сфера: %s\n"+
-            "Описание: %s\n"+
-            "Статус: %s\n"+
-            "Время: %s",
-        request.RequestID,
-        request.BuildingID,
-        request.Photos,
-        field,
-        request.AdditionalText,
-        request.Status,
-        request.Time.Format(time.RFC1123),
-    )
+	msg := fmt.Sprintf(
+		"Заявка #%d создана!\n"+
+			"Здание: %d\n"+
+			"URL фото: %s\n"+
+			"Сфера: %s\n"+
+			"Описание: %s\n"+
+			"Статус: %s\n"+
+			"Время: %s",
+		request.RequestID,
+		request.BuildingID,
+		request.Photos,
+		field,
+		request.AdditionalText,
+		request.Status,
+		request.Time.Format(time.RFC1123),
+	)
 
-
-
-    return c.Send(msg)
+	return c.Send(msg)
 }
